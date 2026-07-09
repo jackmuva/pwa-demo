f32fe28b921d226aee9eeca93ffe99f1f55ccadb
jackmuva <jackmu@umich.edu>
2026-07-09 17:01:19 -0400

    bluettooth

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-    return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans text-black">
+  const [devices, setDevices] = useState<BluetoothDevice[]>([]);
+  useEffect(() => {
+    const pageDiv = document.getElementById("mainDiv");
+    const handlePointerUp = (event: PointerEvent) => {
+      const blDevices: BluetoothDevice[] = [];
+      console.log("ready");
+      navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+        .then(device => {
+          console.log(device);
+          blDevices.push(device);
+        })
+        .catch(error => { console.error(error); });
+      setDevices(blDevices);
+    }
+
+    pageDiv?.addEventListener('pointerup', handlePointerUp);
+
+    return () => {
+      pageDiv?.removeEventListener("pointerup", handlePointerUp);
+    }
+  }, [])
+  return (
+    <div id="mainDiv" className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans text-black">
       Plaud PWA Demo
+      {devices.map((device) => {
+        return (<div>{device.name}</div>);
+      })}
     </div>
   );
 }
